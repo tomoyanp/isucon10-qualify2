362dfefb1fd9789ce96c624830d1e57d07997f72
tomoyanpy <tomoyanpy@gmail.com>
2022-07-21 12:45:34 +0000

    m

--- mysql/db/0_Schema.sql
+++ mysql/db/0_Schema.sql
@@ -43,7 +43,17 @@ create index chair1 ON isuumo.chair(stock, price, id);
 create index chair2 ON isuumo.chair(price, stock);
 create index chair3 ON isuumo.chair(price, id);
 create index chair4 ON isuumo.chair(popularity_v, id);
+create index chair5 ON isuumo.chair(color, stock);
+create index chair6 ON isuumo.chair(width, stock);
+create index chair7 ON isuumo.chair(height, stock);
+create index chair8 ON isuumo.chair(kind, stock);
 create index estate1 ON isuumo.estate(rent, id);
 create index estate2 ON isuumo.estate(rent);
 create index estate3 ON isuumo.estate (popularity_v, id);
+create index estate4 ON isuumo.estate(latitude, longitude);
+create index estate5 ON isuumo.estate(door_height);
+create index estate6 ON isuumo.estate(door_width);
+create index estate7 ON isuumo.estate(door_height, rent);
+
+
 
